72416a44586a162ad30a4828832dafbfa1500d6e
Eric Callari <ericcallari1@gmail.com>
2018-11-21 22:42:49 -0800

    code cleanup

--- database/seed.js
+++ database/seed.js
@@ -30,42 +30,42 @@ const seedRestaurant = (rows = batchSize) => {
   return dataRestaurant;
 };
 
-// generate user_info table data (10M)
-// const seedUserInfo = (rows = batchSize) => {
-//   const dataUserInfo = [];
-//   for (let i = 0; i < rows; i += 1) {
-//     const user = {};
-//     user.user_id = userInfoId;
-//     userInfoId += 1;
-//     user.user_name = faker.name.findName();
-//     user.user_avatar = faker.image.avatar();
-//     user.location = `${faker.address.city()} ${faker.address.state()}`;
-//     user.number_reviews = _.random(1, 50);
-//     user.number_photos = _.random(1, 40);
-//     dataUserInfo.push(user);
-//   }
-//   return dataUserInfo;
-// };
+generate user_info table data (10M)
+const seedUserInfo = (rows = batchSize) => {
+  const dataUserInfo = [];
+  for (let i = 0; i < rows; i += 1) {
+    const user = {};
+    user.user_id = userInfoId;
+    userInfoId += 1;
+    user.user_name = faker.name.findName();
+    user.user_avatar = faker.image.avatar();
+    user.location = `${faker.address.city()} ${faker.address.state()}`;
+    user.number_reviews = _.random(1, 50);
+    user.number_photos = _.random(1, 40);
+    dataUserInfo.push(user);
+  }
+  return dataUserInfo;
+};
 
-// generate users_review table data (40M)
-// const seedUserReviews = (rows = batchSize) => {
-//   const dataUserReview = [];
-//   for (let i = 0; i < rows; i += 1) {
-//     const reviews = {};
-//     const urlPath = 'https://s3-us-west-1.amazonaws.com/pley-food/';
-//     reviews.id = usersReviewsId;
-//     usersReviewsId += 1;
-//     reviews.user_id = _.random(1, 10000000);
-//     reviews.restaurant_id = _.random(1, 10000000);
-//     reviews.date = faker.date.past();
-//     reviews.date = moment(reviews.date).format('YYYY-MM-DD');
-//     reviews.review_comment = HipsterIpsum.get(1);
-//     reviews.score = `${urlPath}star_${_.random(1, 9)}.png`;
-//     reviews.picture_food = `${urlPath + sprintf('%05s.jpg', _.random(1, 1000))}`;
-//     dataUserReview.push(reviews);
-//   }
-//   return dataUserReview;
-// };
+generate users_review table data (40M)
+const seedUserReviews = (rows = batchSize) => {
+  const dataUserReview = [];
+  for (let i = 0; i < rows; i += 1) {
+    const reviews = {};
+    const urlPath = 'https://s3-us-west-1.amazonaws.com/pley-food/';
+    reviews.id = usersReviewsId;
+    usersReviewsId += 1;
+    reviews.user_id = _.random(1, 10000000);
+    reviews.restaurant_id = _.random(1, 10000000);
+    reviews.date = faker.date.past();
+    reviews.date = moment(reviews.date).format('YYYY-MM-DD');
+    reviews.review_comment = HipsterIpsum.get(1);
+    reviews.score = `${urlPath}star_${_.random(1, 9)}.png`;
+    reviews.picture_food = `${urlPath + sprintf('%05s.jpg', _.random(1, 1000))}`;
+    dataUserReview.push(reviews);
+  }
+  return dataUserReview;
+};
 
 const csvConverter = (arr) => {
   let output = '';
@@ -105,10 +105,7 @@ function createSeedWriter(fileName, total, seedGenerator, cb) {
   return writer;
 }
 
-// const userReviewsWriter = createSeedWriter('users_reviews.csv', 40000000, seedUserReviews);
-// const userInfoWriter = createSeedWriter('user_info.csv', 10000000, seedUserInfo, userReviewsWriter);
-// const restaurantWriter = createSeedWriter('restaurant.csv', 10000000, seedRestaurant, userInfoWriter);
-// restaurantWriter();
-
-const restaurantWriter = createSeedWriter('restaurant.csv', 10000000, seedRestaurant);
+const userReviewsWriter = createSeedWriter('users_reviews.csv', 40000000, seedUserReviews);
+const userInfoWriter = createSeedWriter('user_info.csv', 10000000, seedUserInfo, userReviewsWriter);
+const restaurantWriter = createSeedWriter('restaurant.csv', 10000000, seedRestaurant, userInfoWriter);
 restaurantWriter();
